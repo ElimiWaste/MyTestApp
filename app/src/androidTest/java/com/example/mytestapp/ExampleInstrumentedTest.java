81c62bf4d87933fa713b132d7d9fedf15b94487d
shravyaanisetti <shravya.anisetti@gmail.com>
2021-03-29 09:33:18 -0400

    Testing

--- app/src/androidTest/java/com/example/mytestapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mytestapp/ExampleInstrumentedTest.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-/* hi - SA
+/* Hello - SA
 
  */
 @RunWith(AndroidJUnit4.class)
